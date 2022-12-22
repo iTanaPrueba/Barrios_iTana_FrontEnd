63d7b390ebac6ac7f433fc508cee0fec109e0e24
Julio Barrios <juliobarrios0296@gmail.com>
2022-12-22 17:09:58 -0500

    refactor: Se agrego los comandos GET ALL y POST en el servicio

--- src/app/mobile_subscription/service/mobile-subscription.service.ts
+++ src/app/mobile_subscription/service/mobile-subscription.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MobileSubscriptionService {
+  basePath = 'http://localhost:4200/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllMobileSubscription(){
+    return this.http.get(this.basePath + 'mobileSubscriptions');
+  }
+
+  postMobileSubscriptions(data: any){
+    return this.http.post<any>(this.basePath + "mobileSubscriptions", data)
+  }
 }
