483743e0dbc5996af39f84997ce74a1e76736f9a
Julio Barrios <juliobarrios0296@gmail.com>
2022-12-25 20:51:56 -0500

    refactor: Se añadio la validacion del año

--- src/app/mobile_subscription/page/register_mobile_subscription.component.ts
+++ src/app/mobile_subscription/page/register_mobile_subscription.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {mobileSubscriptions} from "../model/mobile_subscription";
 import {MobileSubscriptionService} from "../service/mobile-subscription.service";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'register_mobile_subscription',
@@ -12,12 +13,13 @@ export class Register_mobile_subscriptionComponent implements OnInit {
   _updateDataConfirm!: boolean
   idMobileSubscription!: number
 
-  ngOnInit(): void {
+  ngOnInit(): void{
+
   }
 
   constructor(private formBuilder: FormBuilder, public mobileSubscriptionService: MobileSubscriptionService) {
     this.registerForm = this.formBuilder.group({
-      month: new FormControl('', [Validators.required, Validators.pattern(/^((0[1-9])|(1[0-2]))$/)]),
+      month: new FormControl('', [Validators.required, Validators.pattern(/^(2([0-9]{3})-((0[1-9])|(1[0-2])))$/)]),
       network: new FormControl('', [Validators.required]),
       plan: new FormControl('', [Validators.required]),
       subscriptions: new FormControl('', [Validators.required, Validators.pattern(/^([0-9]+)$/)])
@@ -43,7 +45,7 @@ export class Register_mobile_subscriptionComponent implements OnInit {
 
   updateMobileSubscription(newData: mobileSubscriptions) {
     this.registerForm = this.formBuilder.group({
-      month: new FormControl(newData.month, [Validators.required, Validators.pattern(/^((0[1-9])|(1[0-2]))$/)]),
+      month: new FormControl(newData.month, [Validators.required, Validators.pattern(/^(2([0-9]{3})-((0[1-9])|(1[0-2])))$/)]),
       network: new FormControl(newData.network, [Validators.required]),
       plan: new FormControl(newData.plan, [Validators.required]),
       subscriptions: new FormControl(newData.subscriptions, [Validators.required, Validators.pattern(/^([0-9]+)$/)])
